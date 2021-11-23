b06c069dd17ea6d5c1aa8da955309f7548caa15f
anandgitacc <anand.akmail@gmail.com>
2021-11-23 11:38:28 +0530

    firsttest

---
+++ src/org/test/Testgit.java
@@ -0,0 +1,23 @@
+package org.test;
+
+public class Testgit {
+	
+	public void empName() {
+		
+		System.out.println("anand");
+	}
+	
+	public void empMobno() {
+		
+		System.out.println("8939278836");
+	}
+	
+	public static void main(String[] args) {
+		
+		Testgit t = new Testgit();
+		
+		t.empName();
+		t.empMobno();
+	}
+
+}
